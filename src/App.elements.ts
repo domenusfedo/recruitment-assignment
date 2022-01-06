e41cbb2545a93350e400f9c69c5327d7d7f29f6d
domenusfedo <domanusfedo@gmail.com>
2022-01-06 17:29:09 +0100

    Create a React app and publish it to GitHub Pages

---
+++ src/App.elements.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const AppHolder = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+export const Holder = styled.div`
+
+`
+
+export const SearchField = styled.input`
+
+`
